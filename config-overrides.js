373e13b47e0f4d417ea4a3d4dae446f108b7979a
LouizEchi <louizechiverri@gmail.com>
2019-11-22 18:44:42 +0800

    build(config): restructure config

--- config-overrides.js
+++ config-overrides.js
@@ -20,6 +20,7 @@ module.exports = {
             '@assets': path.resolve(__dirname, 'src/assets/'),
             '@components': path.resolve(__dirname, 'src/components/'),
             '@constants': path.resolve(__dirname, 'src/constants/*'),
+            '@containers': path.resolve(__dirname, 'src/containers/*'),
         }),
 
         // adjust the underlying workbox
